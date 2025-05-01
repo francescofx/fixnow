25d3f9b0680e1758329ebd8e818d1cde29d328ed
francescofx <francescomarreddaao@gmail.com>
2025-05-01 10:26:40 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RequestHelp from './pages/RequestHelp';
-import ConfirmationPage from './pages/ConfirmationPage'; // 👈 aggiunto
+import ConfirmationPage from './pages/ConfirmationPage';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import DashboardUser from './pages/DashboardUser';
+import ProtectedRoute from './pages/ProtectedRoute'; // Protezione tecnica
 
 function App() {
   return (
@@ -10,7 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/richiesta" element={<RequestHelp />} />
-        <Route path="/conferma" element={<ConfirmationPage />} /> {/* 👈 aggiunto */}
+        <Route path="/conferma" element={<ConfirmationPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/dashboard-utente" element={<DashboardUser />} />
       </Routes>
     </Router>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,15 @@
-// src/firebase.js
-
 // Importa i moduli principali
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth"; // 👈 AGGIUNTO
 
 // La tua configurazione Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBy9Y2ahJdmj66A0Rz-rSjkeeLwASHO_Jc",
   authDomain: "fixnow-22379.firebaseapp.com",
   projectId: "fixnow-22379",
-  storageBucket: "fixnow-22379.appspot.com",  // ⚡️ corretto: manca una "p" rispetto a prima
+  storageBucket: "fixnow-22379.appspot.com",
   messagingSenderId: "54994993133",
   appId: "1:54994993133:web:8d28f1f33519420519f1e4"
 };
@@ -24,5 +23,8 @@ const db = getFirestore(app);
 // Storage per foto
 const storage = getStorage(app);
 
-// Esporta db e storage
-export { db, storage };
+// Auth per login
+const auth = getAuth(app);
+
+// Esporta db, storage, auth
+export { db, storage, auth };
